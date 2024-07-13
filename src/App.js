40504b26f620ee725deb29449af4a33e6ea3071a
saga-r-code <shah.sagar2006@gmail.com>
2024-07-13 18:51:12 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [nasaphotos, setnasaphotos] = useState([])
+
+  const updatephoto = async () =>{
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=VE9UeKOWxelQq1TyXk4ghx6HBrzfk185Smdc519n')
+    try{
+    const data = await response.json()
+    setnasaphotos([data])
+    }
+    catch(error){
+      console.log(error)
+      }
+  }
+
+  useEffect(() => {
+   updatephoto()
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5">
+      <h1 className="text-3xl font-bold underline">NASA - APOD</h1>
+      <div className="cards flex justify-center items-center">
+       {nasaphotos.map((e)=>{
+        return(
+          <div key={e.title} className="p-5 flex gap-3">
+             <div className="image ">
+         <img src={e.url} alt={e.title} className=" rounded-xl" />
+            </div>
+            <div className="details">
+            <h1 className="text-2xl">{e.title}</h1>
+            <p className="text-slate-500">{e.explanation}</p>
+            <p>{e.date}</p>
+            </div>
+         </div>
+        )
+       })}
+      </div>
     </div>
   );
 }
---
+++ src/Comoponents/NasaPhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NasaPhoto = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NasaPhoto
